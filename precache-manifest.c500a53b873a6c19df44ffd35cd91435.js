e87d7dc88426df15a9823a199536b0e035cda3e7
JohnVemula1998 <johnvemula7771998@gmail.com>
2022-02-06 17:19:54 +0530

    Updates

---
+++ precache-manifest.c500a53b873a6c19df44ffd35cd91435.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "576640a311aaac8d4b86",
+    "url": "/reactweektest/static/css/main.afd39272.chunk.css"
+  },
+  {
+    "revision": "576640a311aaac8d4b86",
+    "url": "/reactweektest/static/js/main.3c421f94.chunk.js"
+  },
+  {
+    "revision": "457bb18c278399686025",
+    "url": "/reactweektest/static/js/runtime~main.6f46a0cb.js"
+  },
+  {
+    "revision": "0e4007777a51aae15166",
+    "url": "/reactweektest/static/js/2.cf2742ac.chunk.js"
+  },
+  {
+    "revision": "4859a0e94443de56ed48bce0f11ed04c",
+    "url": "/reactweektest/index.html"
+  }
+];
